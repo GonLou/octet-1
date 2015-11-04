ab653646a1e516f1c15d453805dafcdb898cf798
GonLou <gnunodal@hotmail.com>
2015-11-04 12:12:38 +0000

    sites added as cylinders but plane is missing on laterals

--- octet/src/examples/pro_city/pro_city.h
+++ octet/src/examples/pro_city/pro_city.h
@@ -4,11 +4,21 @@
 //
 // Modular Framework for OpenGLES2 rendering on multiple platforms.
 //
+using namespace std;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 namespace octet {
   /// Scene containing a box with octet.
   class pro_city : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+
+	dynarray<vec3p> sites;
+
+
   public:
     /// this is called when we construct the class before everything is initialised.
     pro_city(int argc, char **argv) : app(argc, argv) {
@@ -19,21 +29,45 @@ namespace octet {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
 	  app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 4, 0));
+	  //app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 120, 0));
+	  //app_scene->get_camera_instance(0)->get_node()->rotate(-45,vec3(10, 0, 0));
 
       material *red = new material(vec4(1, 0, 0, 1));
 	  material *brown = new material(vec4(0, 1, 0, 1));
+	  material *white = new material(vec4(0, 0, 0, 1));
+	  material *black = new material(vec4(1, 1, 1, 1));
 	  
 	  mat4t mat;
+
+	  // ground
+	  mat.loadIdentity();
+	  mat.translate(0, -1, 0);
+	  //mat.rotate(5,1,0,0);
+	  app_scene->add_shape(mat, new mesh_box(vec3(200, 1, 200)), brown, false);
 
 	  // just a ball
 	  mat.translate(0, 12, 0);
 	  app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), red, true);
 
-	  // ground
-	  mat.loadIdentity();
-	  mat.translate(0, -1, 0);
-	  mat.rotate(5,1,0,0);
-	  app_scene->add_shape(mat, new mesh_box(vec3(200, 1, 200)), brown, false);
+	  printf("adding sites...\n");
+	  srand(100);
+	  for (int i = 0; i < 10; i++) {
+		  //sites.push_back(vec3(rand() % 100, 0, rand() % 100));
+		  int x = rand() % 100;
+		  int z = rand() % 100;
+		  printf("x: %d | z: %d\n", x, z);
+		  // adding sites as cylinders
+		  //sites.end;
+		  mat.translate(x, 0, z);
+		  app_scene->add_shape(mat, new mesh_cylinder(vec3(2, 2, 2), 2), red, true);
+	  }
+
+	  //polygon
+	  //polygon top_level;
+	  //top_level.add_vertex(vec3(-1000, 0, -1000));
+	  //top_level.add_vertex(vec3(1000, 0, -1000));
+	  //top_level.add_vertex(vec3(1000, 0, 1000));
+	  //top_level.add_vertex(vec3(-1000, 0, 1000));
     }
 
     /// this is called to draw the world
@@ -42,8 +76,13 @@ namespace octet {
       get_viewport_size(vx, vy);
       app_scene->begin_render(vx, vy);
 
+	  scene_node *cam_node = app_scene->get_camera_instance(0)->get_node();
+
+
 	  {
-		  scene_node *cam_node = app_scene->get_camera_instance(0)->get_node();
+		  
+
+
 		  if (app::is_key_down(key_up)) {
 			  cam_node->translate(vec3(0, 0, -1));
 		  }
@@ -56,6 +95,8 @@ namespace octet {
 		  if (app::is_key_down(key_right)) {
 			  cam_node->rotate(-5, vec3(0, 1, 0));
 		  }
+
+
 	  }
 
       // update matrices. assume 30 fps.
@@ -65,7 +106,7 @@ namespace octet {
       app_scene->render((float)vx / vy);
 
       // tumble the box  (there is only one mesh instance)
-      scene_node *node = app_scene->get_mesh_instance(0)->get_node();
+      //scene_node *node = app_scene->get_mesh_instance(0)->get_node();
 
     }
   };
